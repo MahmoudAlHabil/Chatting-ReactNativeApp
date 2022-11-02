caab4b7431624bc7b173864656ef7d9e208867f0
Mahmoud AlHabil <mahmoudalhabil2001@gmail.com>
2022-11-02 12:25:32 +0200

    Add navigation, utils and fonts

---
+++ src/navigations/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStack from './RootStack';
+
+const AppContainer = () => {
+
+  return (
+    <NavigationContainer>
+        <RootStack/>
+    </NavigationContainer>
+  );
+}
+
+export default AppContainer;
