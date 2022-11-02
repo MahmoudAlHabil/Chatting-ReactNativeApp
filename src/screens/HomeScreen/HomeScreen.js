a12d981fb9613c8c1fd98c7c855dc8b4583087cf
Hadeel Alshaikh Ali <haddelayman4@gmail.com>
2022-11-02 14:08:17 +0300

    add home screen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
+import {View, Image, Text} from 'react-native';
 import React from 'react'
 import styles from './styles'
+const img = require('../../utils/images/girl.jpg');
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <View style={styles.headerContainer}>
+        <Image source={img} style={styles.ImgHeader} />
+        <Text>Chat</Text>
+      </View>
     </View>
   )
 }
